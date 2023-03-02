2904345e5ffec51f7993e3b9f2b3876269761e81
lokot0k <kaptoxa322@mail.ru>
2023-03-02 22:54:19 +0300

    Focus changing(macOS) rework for proper generated-text paster

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Just clone repo
 ## Improvements ##
 - [X] script for pasting
 - [X] image-to-text with tesseract
-- [ ] Proper pasting with enter+shift instead nl
+- [ ] Proper pasting with enter+shift instead nl (unnecceary)
+- [ ] Deal with markers from pictures
 - [ ] UI for pasting (PyQt6 prolly/Terminal util)
-- [ ] hot-key for pasting/think about changing focus on avrora
+- [X] hot-key for pasting/think about changing focus on avrora on macOS
+- [ ] do previous task on windows
+- [ ] do previous task on linux
--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import time
 from PIL import Image
-import pytesseract as pytesseract
-from pynput.keyboard import Key, Controller
+import pytesseract
+from pynput.keyboard import Controller
+from os import system
+from platform import system as platform
 
-spec_chars = {"†": "tab",
-              "˜": "nl"}
 
-input_file = input()
-custom_config = r'-l rus+eng --psm 6'
-txt = pytesseract.image_to_string(Image.open(f'{input_file}'),
-                                  config=custom_config)
+def focus_change():
+    if platform() == 'Darwin':
+        system(
+            '''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "ARM_Student" to true' '''
+        )
 
-txt = txt.replace("\t", "†")
-txt = txt.replace("\n", "˜")
 
-time.sleep(3)  # время на смещение фокуса
-keyb = Controller()
-for i in txt:
-    time.sleep(0.1)
-    if i in spec_chars.keys():
-        match i:
-            case "†":
-                for _ in range(4):
-                    keyb.press(" ")
-                    keyb.release(" ")
-            case "˜":
-                keyb.press(Key.enter)
-                keyb.release(Key.enter)
-    else:
-        try:
-            keyb.press(i)
-            keyb.release(i)
-        except:
-            print(i)
+def main():
+    input_file = input()
+    custom_config = r'-l rus+eng --psm 6'
+    txt = pytesseract.image_to_string(Image.open(f'{input_file}'),
+                                      config=custom_config)
+
+    txt = txt.split("\n")
+    focus_change()
+    keyb = Controller()
+
+    # Парсинг для метода решения
+    for line in txt:
+        is_prefix = False
+        if ":" not in line:
+            is_prefix = True
+        if is_prefix:
+            for _ in range(4):
+                keyb.press(" ")
+                keyb.release(" ")
+        for letter in line:
+            time.sleep(0.1)
+            try:
+                keyb.press(letter)
+                keyb.release(letter)
+            except (Controller.InvalidKeyException, ValueError):
+                print(letter)
+        keyb.type("\n")
+
+
+if __name__ == "__main__":
+    main()
